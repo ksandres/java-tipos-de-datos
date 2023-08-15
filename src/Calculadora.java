7c074d8b67d060aae471fd062a32ce1a1d4b9bb6
ksandres <anfega3@gmail.com>
2023-08-15 15:21:11 -0500

    se agregaron operaciones

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -12,30 +12,37 @@ public class Calculadora {
         char operacion = lector.next().charAt(0);
 
         switch (operacion) {
-            case '+' :
+            case '+' -> {
                 // Operación suma:
                 int suma = numero1 + numero2;
                 System.out.println("La suma es " + suma);
-                break;
-            case '-' :
+            }
+            case '-' -> {
                 // Operación resta:
                 int resta = numero1 - numero2;
                 System.out.println("La resta es " + resta);
-                break;
-            case '*' :
+            }
+            case '*' -> {
                 // Operación multiplicación:
                 int multiplicacion = numero1 * numero2;
                 System.out.println("La multiplicación es " + multiplicacion);
-                break;
-            case '/' :
+            }
+            case '/' -> {
                 // Operación división:
                 /* Es necesario que uno de los números que intervienen en la división sea double
                 para que el resultado decimal sea aproximado correctamente. **/
                 double division = (double) numero1 / numero2;
                 System.out.println("La división es " + division);
-                break;
-            default:
-                System.out.println("Operación inválida");
+            }
+            case '%' -> {
+                float porcentaje = (float) (numero1 * numero2) / 100;
+                System.out.println("el porcentaje es" + porcentaje);
+            }
+            case '^' -> {
+                int potencia = (int) Math.pow(numero1, numero2);
+                System.out.println("el resultado de la potencia es" + potencia);
+            }
+            default -> System.out.println("Operación inválida");
         }
     }
 }
